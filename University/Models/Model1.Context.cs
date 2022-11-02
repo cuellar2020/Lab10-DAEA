f88daa26aab80cbebf4a9753dc74c9e5025239e5
cuellar2020 <67763251+cuellar2020@users.noreply.github.com>
2022-11-02 18:31:34 -0500

    Add files via upload

---
+++ University/Models/Model1.Context.cs
@@ -0,0 +1,24 @@
+﻿
+namespace University.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    
+    public partial class UniversityDBEntities : DbContext
+    {
+        public UniversityDBEntities()
+            : base("name=UniversityDBEntities")
+        {
+        }
+    
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+    
+        public virtual DbSet<Course> Course { get; set; }
+        public virtual DbSet<Enrollment> Enrollment { get; set; }
+        public virtual DbSet<Student> Student { get; set; }
+    }
+}
